cb84be66ef9afe09cffc78a399570121c3fb5f0e
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2026-05-18 16:00:05 +0545

    feat: initialize home page component with basic Tailwind styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ import React from 'react'
 export default function Page() {
   return (
     <>
-      <h1 className="text-h1 font-medium">Heading</h1>
-      <p className="text-t2">Paragraph</p>
-
-      <button className="text-b1 font-medium bg-primary text-white">
-        Button
-      </button>
-
-      <label className="text-l2 font-semibold">
-        Label
-      </label>
+      <h1 className="text-h1 font-medium text-primary justify-center flex">Tailwind Worked</h1>
     </>
   )
 }
